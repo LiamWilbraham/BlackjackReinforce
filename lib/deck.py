3049e59d29937c84ea98b88f779b8818c3abaaac
Andrew D. McGuire <andrew.mcguire@spirocontrol.com>
2018-07-22 22:33:47 +0100

    cleaning up the deck class

--- lib/deck.py
+++ lib/deck.py
@@ -5,8 +5,8 @@ class Deck:
         
         '''A simple deck class'''
         
-        suit = {'2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '10':10, 'J':10, 'Q':10, 'K':10, 'A':11} 
-        self.cards = random.sample(list(suit.keys())*4, 52)
+        suit = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+        self.cards = random.sample(suit*4, 52)
     
     def deal(self, n):
         # return the first n cards in the deck
